82b05a56eab2c34afd2d1eda6715d7bdd8b179ec
Carlos Henrique <carlos.henrique.s.j@gmail.com>
2021-06-15 13:53:33 -0300

    final primeira aula

---
+++ CursoMVCDIO/CursoMVCDIO/Models/Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVCDIO.Models
+{
+    public class Context :DbContext
+
+    {
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+            //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CursoMVC;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CursoMVCDIO;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            optionsBuilder.UseSqlServer(connectionString: @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CursoMVC;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+
+    }
+}
